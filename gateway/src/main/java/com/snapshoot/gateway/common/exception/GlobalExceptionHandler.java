0418eb97336d771d0fd05fd7aa13d8e5418cb1a1
FSXplay <skinoftheduck@gmail.com>
2026-08-16 20:53:36 +0800

    add basic exception handler

---
+++ gateway/src/main/java/com/snapshoot/gateway/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.snapshoot.gateway.common.exception;
+
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(
+        GlobalExceptionHandler.class
+    );
+
+    private record ErrorResponse(String message) {
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(
+        NotFoundException ex
+    ) {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    // This 400 error is raised intentionally by the developer
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(
+        BadRequestException ex
+    ) {
+        return ResponseEntity
+            .badRequest()
+            .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    // This 400 error is raised by the Jakarta DTO validation
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+        MethodArgumentNotValidException ex
+    ) {
+        String message = ex
+            .getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+            .collect(Collectors.joining(", "));
+
+        return ResponseEntity
+            .badRequest()
+            .body(new ErrorResponse(message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalException(
+        Exception ex
+    ) {
+        logger.error("Unknown internal error", ex);
+
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse("Internal server error"));
+    }
+}
---
+++ gateway/src/main/java/com/snapshoot/gateway/common/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.snapshoot.gateway.common.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
